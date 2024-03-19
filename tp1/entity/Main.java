a0f01942b47e1ac81e43f8f037e750658c56a449
tomasgrau2 <t.grau@alumno.um.edu.ar>
2024-03-19 19:04:17 -0300

    TP 1.1

---
+++ tp1/entity/Main.java
@@ -0,0 +1,8 @@
+package entity;
+
+public class Main {
+    public static void main(String[] args) {
+        Ingrediente ingrediente1 = new Ingrediente("Huevo", 12);
+        System.out.println(ingrediente1);
+    }
+}
